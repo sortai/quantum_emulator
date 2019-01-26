e5c1fda495c5681b8bdefd165965c022adc43865
sortai <32301806+sortai@users.noreply.github.com>
2019-01-26 08:24:14 +0100

    qsys.__init__, qsys.__len__

--- quem.py
+++ quem.py
@@ -2,4 +2,12 @@ import numpy as np
 
 class qsys:
     def __init__(self, nstates = 2, initstate = None):
-        pass
+        if initstate is None:
+            initstate = np.zeros((nstates,), dtype=np.complex128)
+            initstate[0] = 1+0j
+            self.state = initstate
+        else:
+            self.state = initstate.copy()
+    def __len__(self):
+        return len(self.state)
+        
