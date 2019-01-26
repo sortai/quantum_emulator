dd529752c94eccab97dc6dcc0af3ac56300f6d38
sortai <32301806+sortai@users.noreply.github.com>
2019-01-26 08:28:17 +0100

    Added exceptions.py
    also added a check in qsys.__init__

--- quem.py
+++ quem.py
@@ -1,4 +1,5 @@
 import numpy as np
+from exceptions import *
 
 class qsys:
     def __init__(self, nstates = 2, initstate = None):
@@ -7,6 +8,7 @@ class qsys:
             initstate[0] = 1+0j
             self.state = initstate
         else:
+            if len(initstate) != nstates: raise NumberOfStatesError("len(initstate) != nstates")
             self.state = initstate.copy()
     def __len__(self):
         return len(self.state)
